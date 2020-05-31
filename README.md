9173d3e2cb39ac35d5069370ea770a331f7c8c7c
op7ic <3172590+op7ic@users.noreply.github.com>
2020-05-31 22:21:40 +0300

    specific event hunt script added

--- README.md
+++ README.md
@@ -105,6 +105,133 @@ Sample output:
                          Host: testbox.amp.local URL: http://mirrorlist.centos.org/?release=7&arch=x86_64&repo=extras&infra=stock
 ```
 
+
+## getSpecificEvent.py
+
+This file extracts specific events and exports them to CSV file with the event number as file name.
+
+How to invoke:
+
+```
+python3 getSpecificEvent.py <event_number> <CSV file name>
+i.e.
+python3 getSpecificEvent.py 1107296274 cloud.ioc.csv
+```
+
+Example output:
+```
+[+] Total results: 3452
+[+] Event Type: Cloud IOC
+[+] Dumped 3331 events to cloud.ioc.csv
+```
+
+List of available event codes can be retrieved using [API function event_types](https://api-docs.amp.cisco.com/api_actions/details?api_action=GET+%2Fv1%2Fevent_types&api_host=api.amp.cisco.com&api_resource=Event+Type&api_version=v1):
+
+```
+553648130 : Policy Update
+554696714 : Scan Started
+554696715 : Scan Completed, No Detections
+1091567628 : Scan Completed With Detections
+2165309453 : Scan Failed
+1090519054 : Threat Detected
+553648143 : Threat Quarantined
+2164260880 : Quarantine Failure
+570425394 : Quarantine Restore Requested
+553648149 : Quarantined Item Restored
+2164260884 : Quarantine Restore Failed
+2181038130 : Quarantine Request Failed to be Delivered
+553648154 : Cloud Recall Restore from Quarantine
+553648155 : Cloud Recall Quarantine Successful
+2164260892 : Cloud Recall Restore from Quarantine Failed
+2164260893 : Cloud Recall Quarantine Attempt Failed
+553648158 : Install Started
+2164260895 : Install Failure
+553648166 : Uninstall
+2164260903 : Uninstall Failure
+1003 : Email Confirmation
+1004 : Forgotten Password Reset
+1005 : Password Has Been Reset
+2164260866 : Policy Update Failure
+553648146 : Cloud Recall Restore of False Positive
+553648147 : Cloud Recall Detection
+553648168 : Execution Blocked
+553648150 : Quarantine Restore Started
+570425396 : Application Registered
+570425397 : Application Deregistered
+570425398 : Application Authorized
+570425399 : Application Deauthorized
+1090524040 : APK Threat Detected
+1090524041 : APK Custom Threat Detected
+1090519084 : DFC Threat Detected
+1107296261 : Adobe Reader compromise
+1107296262 : Microsoft Word compromise
+1107296263 : Microsoft Excel compromise
+1107296264 : Microsoft PowerPoint compromise
+1107296266 : Adobe Reader launched a shell
+1107296267 : Microsoft Word launched a shell
+1107296268 : Microsoft Excel launched a shell
+1107296269 : Microsoft PowerPoint launched a shell
+1107296270 : Apple QuickTime compromise
+1107296271 : Apple QuickTime launched a shell
+1107296272 : Executed malware
+1107296273 : Suspected botnet connection
+553648170 : Reboot Pending
+553648171 : Reboot Completed
+1107296274 : Cloud IOC
+1107296275 : Microsoft Calculator compromise
+1107296276 : Microsoft Notepad compromise
+553648173 : File Fetch Completed
+2164260910 : File Fetch Failed
+554696756 : Endpoint IOC Scan Started
+554696757 : Endpoint IOC Scan Completed, No Detections
+1091567670 : Endpoint IOC Scan Completed With Detections
+2165309495 : Endpoint IOC Scan Failed
+2164260914 : Endpoint IOC Definition Update Failure
+553648179 : Endpoint IOC Definition Update Success
+2164260911 : Endpoint IOC Configuration Update Failure
+553648176 : Endpoint IOC Configuration Update Success
+1090519089 : Endpoint IOC Scan Detection Summary
+1107296277 : Connection to suspicious domain
+1107296278 : Threat Detected in Low Prevalence Executable
+1107296279 : Vulnerable Application Detected
+1107296280 : Suspicious Download
+1107296281 : Microsoft CHM Compromise
+1107296282 : Suspicious Cscript Launch
+1090519096 : Update: Reboot Required
+1090519097 : Update: Reboot Advised
+2164260922 : Update: Unexpected Reboot Required
+553648137 : Product Update Failed
+553648135 : Product Update Started
+553648136 : Product Update Completed
+1107296285 : Cognitive Incident
+1107296284 : Potential Ransomware
+1107296283 : Possible Webshell
+1090519103 : Exploit Prevention
+2164260931 : Critical Fault Raised
+1090519107 : Major Fault Raised
+553648195 : Minor Fault Raised
+553648196 : Fault Cleared
+1090519081 : Rootkit Detection
+1090519105 : Malicious Activity Detection
+1090519102 : iOS Network Detection
+553648199 : Malicious Activity Block
+1090519112 : System Process Protection
+553648202 : Endpoint Isolation Start Success
+2164260939 : Endpoint Isolation Start Failure
+553648204 : Endpoint Isolation Stop Success
+2164260941 : Endpoint Isolation Stop Failure
+553648206 : Endpoint Isolation Update Success
+2164260943 : Endpoint Isolation Update Failure
+553648208 : Orbital Install Success
+2164260945 : Orbital Install Failure
+553648210 : Orbital Update Success
+2164260947 : Orbital Update Failure
+553648215 : Endpoint Isolation Unlock Limit Reached
+1107296257 : Potential Dropper Infection
+1107296258 : Multiple Infected Files
+1107296344 : SecureX Threat Hunting Incident
+```
+
 ## Limitations
 
 AMP timeline allows to search only last 500 events so historical data might be limited
@@ -115,14 +242,19 @@ AMP timeline allows to search only last 500 events so historical data might be l
 - ```api.amp.cisco.com``` - AMP
 - ```api.apjc.amp.cisco.com``` - AMP APJC
 
-## Examples of useful things to grep for
+## Examples of useful things to grep for:
 ```
 rundll32 + url.dll
 net + admin or domain
+net + use of weak credentials to mount network share
 powershell + iex
 powershell + "==" (for base64)
 cmd + whoami
 net + use + http or \\ (for webdav)
+psexec + use of -s or cmd with password
+procdump + lsass
+psexec-svc
+net + administrator
 ```
 
 ## TODO
---
+++ getSpecificEvent.py
@@ -0,0 +1,174 @@
+# This script is based on https://github.com/CiscoSecurity/amp-01-basics/blob/master/04_get_events.py
+# This script is based on https://stackoverflow.com/questions/41180960/convert-nested-json-to-csv-file-in-python
+import sys
+import requests
+import time
+import csv
+import gc
+
+# Ignore insecure cert warnings (enable only if working with onsite-amp deployments)
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# Get all leaves from JSON file so headers can be used for CSV
+def get_leaves(item, key=None):
+    """ Idea taken from https://stackoverflow.com/questions/41180960/convert-nested-json-to-csv-file-in-python """ 
+    if isinstance(item, dict):
+        leaves = []
+        for i in item.keys():
+            leaves.extend(get_leaves(item[i], i))
+        return leaves
+    elif isinstance(item, list):
+        leaves = []
+        for i in item:
+            leaves.extend(get_leaves(i, key))
+        return leaves
+    else:
+        return [(key, item)]
+
+# Perfrom reduction on Tuple to ensure that single key is present with multiple values as opposed to multiple keys
+def reduceTuple(input_list): 
+    """ Idea taken from https://www.geeksforgeeks.org/python-group-tuples-in-list-with-same-first-value/ """ 
+    out = {} 
+    for elem in input_list: 
+        try: 
+            out[elem[0]].extend(elem[1:]) 
+        except KeyError: 
+            out[elem[0]] = list(elem) 
+
+    return [tuple(values) for values in out.values()] 
+
+# Walk and sort JSON
+def walk_json(event):
+    """ Walk JSON to determine sensible fields to extract """
+    """ Idea taken from https://stackoverflow.com/questions/41180960/convert-nested-json-to-csv-file-in-python """ 
+    # Extract headers from JSON object
+    return sorted(get_leaves(event))
+
+
+client_id = 'XXXXXXXXXXXXXXX'# INSERT YOU API KEY
+api_key = 'XXXXXXXXXXXXXXX'# INSERT YOU API KEY
+domainIP = 'XXXXXXXXXXXXXXX' # INSERT YOUR DOMAIN NAME/HOSTNAME WHERE AMP EXISTS
+
+# Validate a command line parameter was provided
+if len(sys.argv) < 2:
+    sys.exit('Usage: <event id> <csv file to write>\n %s' % sys.argv[0])
+
+# Store the command line parameter
+searchEventId=sys.argv[1]
+fileDump=sys.argv[2]
+# Store GUIDs types
+computer_guids = {}
+objects_to_write = {}
+header = {}
+fieldnames = set()
+# Creat session object
+# http://docs.python-requests.org/en/master/user/advanced/
+# Using a session object gains efficiency when making multiple requests
+session = requests.Session()
+session.auth = (client_id, api_key)
+
+# Define URL and parameters
+events = 'https://{}/v1/events?event_type[]={}'.format(domainIP,searchEventId)
+# Query API
+response_events = session.get(events, verify=False)
+
+# Get Headers
+headers=response_events.headers
+# Ensure we don't cross API limits, sleep if we are approaching limits
+if int(headers['X-RateLimit-Remaining']) < 10:
+    print("[+] Sleeping {} seconds to ensure reset clock works".format(int(headers['X-RateLimit-Reset'])))
+    time.sleep(int(headers['X-RateLimit-Reset'])+5)
+# Attempt to get valid JSON object, pass on error 
+try:
+    # Decode JSON response
+    response_event_json = response_events.json()
+    # Name data section of JSON
+    data = response_event_json['data']
+    # Name total section of JSON
+    total = response_event_json['metadata']['results']['total']    
+except:
+    pass
+
+# Print total number of alerts in AMP console
+print("[+] Total results: {}".format(total))
+print("[+] Event Type: {} ".format(data[0]['event_type']))
+# For each record entry, add hostname and GUID of system which matches with Event ID, discard anything else
+for entry in data:
+    if int(entry['event_type_id']) == int(searchEventId) and entry['event_type'] != "Install Started":
+        connector_guid = entry['connector_guid']
+        hostname = entry['computer']['hostname']
+        computer_guids.setdefault(connector_guid, {'hostname':hostname})
+        date=entry['date']
+        # We save GUID + Date to make unique key in the dictionary
+        objects_to_write.setdefault(str(connector_guid+"+"+date),walk_json(entry))
+    else:
+        pass
+
+# If we encounter 500+ events, walk the subsequent pages, not split into separate function for simplicity
+while 'next' in response_event_json['metadata']['links']:
+    # Retrieve next URL link
+    next_url = response_event_json['metadata']['links']['next']
+    response_events = session.get(next_url,verify=False)
+    response_event_json = response_events.json()
+    # Ensure we don't cross API limits, sleep if we are approaching limits
+    headers=response_events.headers
+    if int(headers['X-RateLimit-Remaining']) < 10:
+        print("[+] Sleeping {} seconds to ensure reset clock works".format(int(headers['X-RateLimit-Reset'])))
+        time.sleep(int(headers['X-RateLimit-Reset'])+5)
+        
+    data = response_event_json['data']
+    # For each record entry, add hostname and GUID of system which matches with Event ID, discard anything else
+    for entry in data:
+        if int(entry['event_type_id']) == int(searchEventId) and entry['event_type'] != "Install Started":
+            connector_guid = entry['connector_guid']
+            hostname = entry['computer']['hostname']
+            date=entry['date']
+            computer_guids.setdefault(connector_guid, {'hostname':hostname})
+            # We save GUID + Date to make unique key in the dictionary
+            objects_to_write.setdefault(str(connector_guid+"+"+date),walk_json(entry))    
+        else:
+            pass
+
+# Store headers
+header_set = set()
+# Basic dict structure so we can store data for CSV write
+basic_dict = dict()
+# Store list of dict objects to write back to CSV files
+output_list = list()
+
+# Enum list of uniq objects from events
+for key in objects_to_write:
+    # Reduce the tuple to unique objects which can be sorted later. TODO - write complete reduced object to CSV
+    small_tuple = reduceTuple(objects_to_write[key])
+    # Extract and store specific dictionary elements
+    for z in small_tuple:
+        # Strip and convert only first row (we don't care about additional fields, just want basic ones to show problems)
+        basic_dict.update({z[0]: z[1]})
+    # Write objects to list that we can use to flush down to csvz
+    output_list.append(basic_dict)        
+
+# Make sure all header fields are added
+for d in output_list:
+    # Sort out all headers so they match with whatever headers we get from API
+    for list_keys in d.keys():
+        header_set.add(list_keys)
+
+# Final flush to final output   
+f = open(fileDump, 'w')
+with f:
+    # Use DictWriter to write dictionary write list of objects to csv file
+    writer = csv.DictWriter(f, fieldnames=sorted(header_set))    
+    # Append Header
+    writer.writeheader()
+    # Enumerate list of objects returned
+    for d in output_list:
+        writer.writerow(d)
+f.close()
+
+print("[+] Dumped {} events to {}".format(len(output_list),fileDump))
+# Collect garbage
+gc.collect()
+
+
