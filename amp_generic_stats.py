3d6178fe6e6a77b817308e97157b9020a95d10d4
op7ic <3172590+op7ic@users.noreply.github.com>
2020-06-07 19:54:36 +0300

    Update amp_generic_stats.py

--- amp_generic_stats.py
+++ amp_generic_stats.py
@@ -3,7 +3,9 @@ import requests
 import configparser
 import time
 import gc
-from multiprocessing.pool import ThreadPool
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 # Ignore insecure cert warnings (enable only if working with onsite-amp deployments)
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -11,6 +13,8 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # Containers for GUIDs
 computer_guids = {}
+# Create threadpool instance with 4 workers
+executor = ThreadPoolExecutor(max_workers=4)
 
 def extractGUID(data):
     """ Extract GUIDs from data structure and store them in computer_guids variable"""
@@ -18,6 +22,76 @@ def extractGUID(data):
         connector_guid = entry['connector_guid']
         hostname = entry['hostname']
         computer_guids.setdefault(connector_guid, {'hostname':hostname})
+
+def getStatsOut(guid):
+    """ Function to perform lookup on specific event type"""
+    # Extract trajectory of computers based on their guid
+    trajectory_url = 'https://{}/v1/computers/{}/trajectory'.format(domainIP,guid)
+    trajectory_response = session.get(trajectory_url, verify=False)
+    trajectory_response_json = trajectory_response.json()
+    headers=trajectory_response.headers
+    # Ensure we don't cross API limits, sleep if we are approaching close to limits
+    if int(headers['X-RateLimit-Remaining']) < 10:
+        timeout=int(headers['X-RateLimit-Reset'])
+        time.sleep(timeout+5)
+    # define variables which will be applicable per each GUID
+    vulnerable=0
+    nfm=0
+    executed=0
+    create=0
+    moved=0
+    threat_q=0
+    threat_d=0
+    quarantine_fail=0
+    malicious_activity=0
+    exec_blocked=0
+    exec_malware=0
+    try:
+        events = trajectory_response_json['data']['events']
+        for event in events:
+            event_type = event['event_type']
+            time = event['date'] 
+
+            #filter by specific event type  
+            if event_type == 'Vulnerable Application Detected':
+                vulnerable+=1
+            if event_type == 'NFM':
+                nfm+=1
+            if event_type == 'Executed by':
+                executed+=1
+            if event_type == 'Created by':
+                create+=1
+            if event_type == 'Moved by':
+                moved+=1
+            if event_type == 'Threat Quarantined':
+                threat_q+=1
+            if event_type == 'Threat Detected':
+                threat_d+=1
+            if event_type == 'Quarantine Failure':
+                quarantine_fail+=1
+            if event_type == 'Malicious Activity Detection':
+                malicious_activity+=1
+            if event_type == 'Execution Blocked':
+                exec_blocked+=1
+            if event_type == 'Executed Malware':
+                exec_malware+=1
+        print("{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(
+            time,
+            guid,
+            computer_guids[guid]['hostname'],
+            vulnerable,
+            nfm,
+            executed,
+            create,
+            moved,
+            threat_q,
+            threat_d,
+            quarantine_fail,
+            malicious_activity,
+            exec_blocked,
+            exec_malware))
+    except:
+        pass
 
 
 # Validate a command line parameter was provided
@@ -67,74 +141,9 @@ try:
             # Extract
             response_json = response.json()
             extractGUID(response_json['data'])
-            
+    # add tasks to thread pool
     for guid in computer_guids:
-        # Extract trajectory of computers based on their guid
-        trajectory_url = 'https://{}/v1/computers/{}/trajectory'.format(domainIP,guid)
-        trajectory_response = session.get(trajectory_url, verify=False)
-        trajectory_response_json = trajectory_response.json()
-        headers=trajectory_response.headers
-        # Ensure we don't cross API limits, sleep if we are approaching close to limits
-        if int(headers['X-RateLimit-Remaining']) < 10:
-            timeout=int(headers['X-RateLimit-Reset'])
-            time.sleep(timeout+5)
-        # define variables which will be applicable per each GUID
-        vulnerable=0
-        nfm=0
-        executed=0
-        create=0
-        moved=0
-        threat_q=0
-        threat_d=0
-        quarantine_fail=0
-        malicious_activity=0
-        exec_blocked=0
-        exec_malware=0
-        try:
-            events = trajectory_response_json['data']['events']
-            for event in events:
-                event_type = event['event_type']
-                time = event['date'] 
-
-                #filter by specific event type  
-                if event_type == 'Vulnerable Application Detected':
-                    vulnerable+=1
-                if event_type == 'NFM':
-                    nfm+=1
-                if event_type == 'Executed by':
-                    executed+=1
-                if event_type == 'Created by':
-                    create+=1
-                if event_type == 'Moved by':
-                    moved+=1
-                if event_type == 'Threat Quarantined':
-                    threat_q+=1
-                if event_type == 'Threat Detected':
-                    threat_d+=1
-                if event_type == 'Quarantine Failure':
-                    quarantine_fail+=1
-                if event_type == 'Malicious Activity Detection':
-                    malicious_activity+=1
-                if event_type == 'Execution Blocked':
-                    exec_blocked+=1
-                if event_type == 'Executed Malware':
-                    exec_malware+=1
-            print("{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(
-                time,
-                guid,
-                computer_guids[guid]['hostname'],
-                vulnerable,
-                nfm,
-                executed,
-                create,
-                moved,
-                threat_q,
-                threat_d,
-                quarantine_fail,
-                malicious_activity,
-                exec_blocked,
-                exec_malware))
-        except:
-            pass
+        executor.submit(getStatsOut,guid)
+        
 finally:
     gc.collect()
