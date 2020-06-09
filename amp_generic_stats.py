f5b5f827ae906fe265a4bf772e6dc141f1059976
op7ic <3172590+op7ic@users.noreply.github.com>
2020-06-09 23:56:16 +0300

    Update amp_generic_stats.py

--- amp_generic_stats.py
+++ amp_generic_stats.py
@@ -1,5 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 from threading import BoundedSemaphore
+from threading import Event
 import time
 import sys
 import requests
@@ -66,27 +67,40 @@ def getStatsOut(guid):
     # Extract trajectory of computers based on their guid
     try:
         trajectory_url = 'https://{}/v1/computers/{}/trajectory'.format(domainIP,guid)
-        # Wait 90 seconds to return results therefore becoming a blocking
+        # Wait up to 90 seconds to return results
         # That said, no request should need 90 turnaround unless you query via satelite link
         trajectory_response = session.get(trajectory_url, verify=False,timeout=90)
-        # Extract headers (these are also returned)
-        headers=trajectory_response.headers
-        # In theory we should never reach point this because job scheduler below should take care of measuring API consumption
-        # If we do reach this, we can have multiple threads sleeping at the same time since they all hit the same function. That's OK
-        # We stop on 45 due to number of threads working
-        if 'X-RateLimit-Reset' and 'X-RateLimit-Remaining' in str(headers):
-            if(int(headers['X-RateLimit-Remaining']) < 45):
-                if(headers['Status'] == "200 OK"):
-                    # We are close to the border, in theory 429 error code should never trigger if we capture this event
-                    time.sleep((int(headers['X-RateLimit-Reset'])+5))
-                if(headers['Status'] == "429 Too Many Requests"):
-                    # Triggered too many request, we need to sleep before it continues
-                    time.sleep((int(headers['X-RateLimit-Reset'])+5))
+        if trajectory_response != None:
+            # Extract headers (these are also returned)
+            headers=trajectory_response.headers
+            # check if we correctly got headers
+            if headers != None:
+                # In theory we should never reach point this because job scheduler below should take care of measuring API consumption
+                # If we do reach this, we can have multiple threads sleeping at the same time since they all hit the same function. That's OK
+                # We stop on 45 due to number of threads working
+                if 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
+                    if(int(headers['X-RateLimit-Remaining']) < 45):
+                        if(headers['Status'] == "200 OK"):
+                            # We are close to the border, in theory 429 error code should never trigger if we capture this event
+                            # For some reason simply using time.sleep does not work very well here
+                            Event().wait((int(headers['X-RateLimit-Reset'])+5))
+                        if(headers['Status'] == "429 Too Many Requests"):
+                            # Triggered too many request, we need to sleep before it continues
+                            # For some reason simply using time.sleep does not work very well here
+                            Event().wait((int(headers['X-RateLimit-Reset'])+5))
+                else:
+                    # We sleep - this could be connection problem, returned headers are messed up
+                    Event().wait(20)
+            else:
+                Event().wait(20)
+                return "0"
         else:
-            # headers are corrupted, so sleep now (could be connection problem)
-            time.sleep(20)
+            # Trajectory is empty, return 0 so job worker will know what to do next
+            Event().wait(20)
+            return "0"
+
     except KeyError:
-        # sometimes AMP server returns misformatted header
+        # sometimes AMP server returns misformatted header in 429 code. 
         pass
 
     # define variables which will be applicable per each GUID
@@ -150,9 +164,11 @@ def getStatsOut(guid):
             exec_malware))
     except:
         pass
+
     if 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
         return headers
     else:
+        # headers are corrupted 
         return "0"
 
 
@@ -186,11 +202,11 @@ try:
             headers=response.headers
             # Ensure we don't cross API limits, sleep if we are approaching close to limits
             if int(headers['X-RateLimit-Remaining']) < 45:
-                timeout=int(headers['X-RateLimit-Reset'])
-                time.sleep(timeout+5)
-            # Extract
+                time.sleep((int(headers['X-RateLimit-Reset'])+5))
+            # Extract GUIDs
             response_json = response.json()
             extractGUID(response_json['data'])
+
     # Executor with limit on the queue by number of GUIDs
     # Limit to 8 threads. Full size of queue is number of computer GUIDs + number of threads
     executor = BoundedExecutor(8, len(computer_guids))
@@ -202,7 +218,7 @@ try:
         count+=1
         # check every 4th thread
         if (count == 3):
-            # .result() function will block thread and wait. so we check only every 4th thread to see if we are still ok to proceed
+            # .result() function will block thread and wait so we check only every 4th thread to see if we are still ok to proceed
             try:
                 headers=future.result()
                 if 'X-RateLimit-Remaining' in str(headers):
@@ -213,9 +229,9 @@ try:
                         if(headers['Status'] == "429 Too Many Requests"):
                             # Triggered too many request, we need to sleep before it continues
                             time.sleep((int(headers['X-RateLimit-Reset'])+5))
-                else:
+                elif headers== "0":
                     # Header object is wrong. This is likley 429 response so sleep extra 10 before moving on to next GUID object
-                    # In theory this shouldn't be reached unless threads somehow skip more than 45 requests
+                    # In theory this shouldn't be reached unless threads somehow skip more than 45 requests or server responds back with junk
                     time.sleep(10)
                     continue
                 # reset counter back to 0
