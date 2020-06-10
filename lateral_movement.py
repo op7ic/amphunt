ff154401d3ee5d684dabf09e7c2cc6782d87b94b
op7ic <3172590+op7ic@users.noreply.github.com>
2020-06-10 17:12:12 +0300

    Update lateral_movement.py

--- lateral_movement.py
+++ lateral_movement.py
@@ -4,7 +4,7 @@ import requests
 import configparser
 import time
 import gc
-from threading import Event
+from urllib.parse import urlparse
 
 # Ignore insecure cert warnings (enable only if working with onsite-amp deployments)
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -17,52 +17,38 @@ def extractGUID(data):
         hostname = entry['hostname']
         computer_guids.setdefault(connector_guid, {'hostname':hostname})
 
+def extractDomainFromURL(url):
+    """ Extract domain name from URL"""
+    return urlparse(url).netloc
 
-def trajectoryRequest(guid):
-    """ re-issue trajectory request"""
-    try:
-        trajectory_url = 'https://{}/v1/computers/{}/trajectory'.format(domainIP,guid)
-        trajectory_response = session.get(trajectory_url, verify=False,timeout=90)
-        if trajectory_response != None:
-            # Extract headers (these are also returned)
-            headers=trajectory_response.headers
-            # check if we correctly got headers
-            if headers != None:
-                # If we do reach this, we can have multiple threads sleeping at the same time since they all hit the same function. That's OK
-                # We stop on 45 due to number of threads working
-                if 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
-                    if(int(headers['X-RateLimit-Remaining']) < 45):
-                        if(headers['Status'] == "200 OK"):
-                            # We are close to the border, in theory 429 error code should never trigger if we capture this event
-                            # For some reason simply using time.sleep does not work very well here
-                            Event().wait((int(headers['X-RateLimit-Reset'])+5))
-                        if(headers['Status'] == "429 Too Many Requests"):
-                            # Triggered too many request, we need to sleep before it continues
-                            # For some reason simply using time.sleep does not work very well here
-                            Event().wait((int(headers['X-RateLimit-Reset'])+5))
-                    else:
-                        return headers
-                elif '503 Service Unavailable' in str(headers):
-                    # 503 sent back - server is starting to close
-                    Event().wait(40)
-                    trajectoryRequest(guid)
-                    return "0"
-                else:
-                    #server closed this connection, wait 40 sec to see if we reopen
-                    Event().wait(40)
-                    # retry
-                    trajectoryRequest(guid)
-                    return "0"
-            else:
-                Event().wait(40)
-                trajectoryRequest(guid)
-                return "0"
-    except KeyError:
-        Event().wait(40)
-        return "0"
-    except:
-        Event().wait(40)
-        return "0"
+def checkAPITimeout(headers, response):
+    """Ensure we don't cross API limits, sleep if we are approaching close to limits"""
+    if response:
+        # Extract headers (these are also returned)
+        headers=response.headers
+        # check if we correctly got headers
+        if headers:
+            # We stop on 45 due to number of threads working
+            if 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
+                if(int(headers['X-RateLimit-Remaining']) < 45):
+                    if(headers['Status'] == "200 OK"):
+                        # We are close to the border, in theory 429 error code should never trigger if we capture this event
+                        # For some reason simply using time.sleep does not work very well here
+                        time.sleep((int(headers['X-RateLimit-Reset'])+5))
+                    if(headers['Status'] == "429 Too Many Requests"):
+                        # Triggered too many request, we need to sleep before it continues
+                        # For some reason simply using time.sleep does not work very well here
+                        time.sleep((int(headers['X-RateLimit-Reset'])+5))
+            elif '503 Service Unavailable' in str(headers):
+                time.sleep(60)
+            else: # we got some new error
+                time.sleep(45)
+        else:
+            # no headers, request probably failed
+            time.sleep(45)
+    else: 
+        print("[-] We are not getting response from server. Quiting")
+        sys.exit(1)
 
 
 # Validate a command line parameter was provided
@@ -94,17 +80,7 @@ try:
     # Get headers from request
     headers=response.headers
     # Ensure we don't cross API limits, sleep if we are approaching close to limits
-    if headers != None and 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
-        if int(headers['X-RateLimit-Remaining']) < 20:
-            # This is last one before it kicks in timeout
-            if(headers['Status'] == "200 OK"):
-                time.sleep((int(headers['X-RateLimit-Reset'])+5))
-            if(headers['Status'] == "429 Too Many Requests"):
-                # Triggered too many request, we need to sleep before it continues
-                time.sleep((int(headers['X-RateLimit-Reset'])+5))
-    else:
-        #probably 503
-        time.sleep(40)
+    checkAPITimeout(headers, response)
         
     # Decode first JSON response
     response_json = response.json()
@@ -116,18 +92,8 @@ try:
             next_url = response_json['metadata']['links']['next']
             response = session.get(next_url)
             headers=response.headers
-            if headers != None and 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
-                if int(headers['X-RateLimit-Remaining']) < 20:
-                    # This is last one before it kicks in timeout
-                    if(headers['Status'] == "200 OK"):
-                        time.sleep((int(headers['X-RateLimit-Reset'])+5))
-                    if(headers['Status'] == "429 Too Many Requests"):
-                        # Triggered too many request, we need to sleep before it continues
-                        time.sleep((int(headers['X-RateLimit-Reset'])+5))
-            else:
-                #probably 503 or server closed connection
-                time.sleep(40)
-            # Extract
+            checkAPITimeout(headers, response)
+            # Extract GUID
             response_json = response.json()
             extractGUID(response_json['data'])
 
@@ -136,80 +102,44 @@ try:
     for guid in computer_guids:
         print('\n\t[+] Querying: {} - {}'.format(computer_guids[guid]['hostname'], guid))
         trajectory_url = 'https://{}/v1/computers/{}/trajectory'.format(domainIP,guid)
-        trajectory_response = session.get(trajectory_url, verify=False,timeout=90)
+        trajectory_response = session.get(trajectory_url, verify=False)
         trajectory_response_json = trajectory_response.json()
-
-        if trajectory_response:
-            # Extract headers (these are also returned)
-            headers=trajectory_response.headers
-            # check if we correctly got headers
-            if headers:
-                # We stop on 45 due to number of threads working
-                if 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(headers):
-                    if(int(headers['X-RateLimit-Remaining']) < 45):
-                        if(headers['Status'] == "200 OK"):
-                            # We are close to the border, in theory 429 error code should never trigger if we capture this event
-                            # For some reason simply using time.sleep does not work very well here
-                            Event().wait((int(headers['X-RateLimit-Reset'])+5))
-                        if(headers['Status'] == "429 Too Many Requests"):
-                            # Triggered too many request, we need to sleep before it continues
-                            # For some reason simply using time.sleep does not work very well here
-                            Event().wait((int(headers['X-RateLimit-Reset'])+5))
-                elif '503 Service Unavailable' in str(headers):
-                    # We sleep - server sent 503 - probably thinking its DDOS. Sleep 40 before moving on 
-                    Event().wait(40)
-                    # re-issue the same trajectory request, hope for headers to come back
-                    h = trajectoryRequest(guid)
-                    # check if we got the right parameters in there
-                    if h != "0" and 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(h):
-                        headers=h
-                    else:
-                        headers="0"
-                else:
-                    # We also need to sleep - server closed connection
-                    # Connection, closed. Similar headers to below:
-                    """{'Cache-Control': 'no-cache', 'Connection': 'close', 'Content-Type': 'text/html'}"""
-                    Event().wait(40)
-                    h = trajectoryRequest(guid)
-                    # check if we got the right parameters in there
-                    if h != "0" and 'X-RateLimit-Remaining' and 'X-RateLimit-Reset' in str(h):
-                        headers=h
-                    else:
-                        headers="0"
-
+        headers=trajectory_response.headers
+        # Ensure we don't cross API limits, sleep if we are approaching close to limits
+        checkAPITimeout(headers, trajectory_response)
+        try:
             events = trajectory_response_json['data']['events']
-            if events != None:
-                for event in events:
-                    event_type = event['event_type']
-                    time = event['date']
-                    if event_type == 'NFM':
-                        network_info = event['network_info']
-                        protocol = network_info['nfm']['protocol']
-                        local_ip = network_info['local_ip']
-                        local_port = network_info['local_port']
-                        remote_ip = network_info['remote_ip']
-                        remote_port = network_info['remote_port']
-                        direction = network_info['nfm']['direction']
-                        if direction == 'Outgoing connection from' and remote_port == '445' or remote_port == '139':
-                            print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound SMB',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Incoming connection from' and local_port =='445' or remote_port =='139':
-                            print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound SMB',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Outgoing connection from' and remote_port == '3389':
-                            print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound RDP',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Incoming connection from' and local_port == '3389':
-                            print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound RDP',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Outgoing connection from' and remote_port == '5985':
-                            print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound WINRM - HTTP',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Incoming connection from' and local_port == '5985':
-                            print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound WINRM - HTTP',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Outgoing connection from' and remote_port == '5986':
-                            print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound WINRM - HTTPS',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Incoming connection from' and local_port == '5986':
-                            print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound WINRM - HTTPS',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Outgoing connection from' and remote_port == '135':
-                            print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound WMIC/SC (RPC)',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
-                        if direction == 'Incoming connection from' and local_port == '135':
-                            print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound WMIC/SC (RPC)',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
+            for event in events:
+                event_type = event['event_type']
+                time = event['date']
+                if event_type == 'NFM':
+                    network_info = event['network_info']
+                    protocol = network_info['nfm']['protocol']
+                    local_ip = network_info['local_ip']
+                    local_port = network_info['local_port']
+                    remote_ip = network_info['remote_ip']
+                    remote_port = network_info['remote_port']
+                    direction = network_info['nfm']['direction']
+                    if direction == 'Outgoing connection from' and remote_port == '445' or remote_port == '139':
+                        print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound SMB',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Incoming connection from' and local_port =='445' or remote_port =='139':
+                        print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound SMB',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Outgoing connection from' and remote_port == '3389':
+                        print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound RDP',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Incoming connection from' and local_port == '3389':
+                        print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound RDP',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Outgoing connection from' and remote_port == '5985':
+                        print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound WINRM - HTTP',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Incoming connection from' and local_port == '5985':
+                        print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound WINRM - HTTP',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Outgoing connection from' and remote_port == '5986':
+                        print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound WINRM - HTTPS',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Incoming connection from' and local_port == '5986':
+                        print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound WINRM - HTTPS',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Outgoing connection from' and remote_port == '135':
+                        print('\t\t\t {} : {} : {} : {} {}:{} -> {}:{}'.format(time,'outbound WMIC/SC (RPC)',computer_guids[guid]['hostname'],protocol,local_ip,local_port,remote_ip,remote_port))
+                    if direction == 'Incoming connection from' and local_port == '135':
+                        print('\t\t\t {} : {} : {} :  {} {}:{} <- {}:{}'.format(time,'inbound WMIC/SC (RPC)',computer_guids[guid]['hostname'], protocol,local_ip,local_port,remote_ip,remote_port))
                         
         else:
             # events array not received - we just pass
